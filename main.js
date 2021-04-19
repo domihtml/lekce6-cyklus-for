7c72cdfc74bcedad1e1b8690eb724f0d64b2eab4
domihtml <dvorak.dominika@gmail.com>
2021-04-19 15:41:58 +0200

    Přepis for cyklů na while cykly

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-// Použij výpis do konzole prohlížeče pomocí console.log('text')
-// Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
-
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 
+console.log('První příklad - Vypiš vzestupně čísla od 0 do 10');
 
 
-//TADY NAPIS CYKLUS
-
-for (let i = 1; i<=10; i = i + 1) {
+/*for (let i = 1; i<=10; i = i + 1) {
 
     console.log(i);
     }
+*/
+
+let i = 1;
+while (i <= 10) {
+    console.log(i++);
+    
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -20,11 +23,16 @@ for (let i = 1; i<=10; i = i + 1) {
 console.log('Druhý příklad - vyzkoušej, jaký je rozdíl mezi <= a<');
 //TADY NAPIS CYKLUS
 
-for (let i = 1; i<10; i = i + 1) {
+/*for (let i = 1; i<10; i = i + 1) {
 
     console.log(i);
 }
-
+*/
+let j = 1;
+while (j < 10) {
+    console.log(j++);
+    
+}
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
@@ -32,7 +40,16 @@ console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
 
-for (let i = 10; i>=0; i = i - 1) {
+
+
+/* for (let i = 10; i>=0; i = i - 1) {
 
     console.log(i);
     }
+
+*/
+
+let k = 10;
+while (k >= 0) {
+    console.log(k--);   
+}
